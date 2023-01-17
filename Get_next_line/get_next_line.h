b4657bdf9f2e91acb9205df27de0cddd00a22599
Thomas Berube-lambert <tberube-@c2r3p1.42quebec.com>
2023-01-17 12:58:35 -0500

    add gnl

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tberube- <tberube-@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 13:39:52 by tberube-          #+#    #+#             */
+/*   Updated: 2022/03/04 10:02:36 by tberube-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int const fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+void	no_end_line(char **save, char *tampon);
+char	*end_fd(char *save, char *tampon, char *tmp);
+int		l_cnt(char *str, int c);
+char	*gratuit(char *detruis);
+int		*line_lenght(char *str, int c);
+
+#endif
