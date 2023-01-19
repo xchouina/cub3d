9196dae8dbfee8a05a7cf29f1a70f583c5924a89
Thomas Berube-lambert <tberube-@c2r3p2.42quebec.com>
2023-01-19 15:09:03 -0500

    bruler le main

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -10,7 +10,7 @@ int	main(int argc, char **argv)
 	// open_fd(argv[1]);
 	if (parsing(&game, fd, argv[1]) == 1)
 		return (1);
-	//print_tab_fd(&game);
+	print_tab_fd(&game);
 	//print_map(&game);
 	return (0);
 }
